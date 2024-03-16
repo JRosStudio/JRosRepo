95551eba2072e488b9b746db67bffc3dcb3c0ee4
JRosStudio <jesusrosc@gmail.com>
2024-03-16 21:40:03 +0100

    Rope Deploy System Start

--- Assets/Scripts/Rope_Fly.cs
+++ Assets/Scripts/Rope_Fly.cs
@@ -7,12 +7,23 @@ public class Rope_Fly : MonoBehaviour
     public Vector3 goal = new Vector3(0,0,0);
     public bool startMovement = false;
     public float speed = 5.0f;
+    [SerializeField]
+    GameObject rope;
+    [SerializeField]
+    GameObject ropeHook;
+
+    Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindAnyObjectByType<Player>();
+    }
     void Update()
     {
         if (startMovement == true) {
            transform.position = Vector3.MoveTowards(transform.position, goal, speed * Time.deltaTime);
             if (transform.position == goal) {
-                Destroy(gameObject);
+                DeployRope();
             }
         }
     }
@@ -27,5 +38,14 @@ public class Rope_Fly : MonoBehaviour
         if (collision.gameObject.tag == "Rope") {
             Destroy(gameObject);
         }
+    }
+
+    public void DeployRope() {
+        GameObject ropeHookInstance = Instantiate(ropeHook, goal, Quaternion.identity);
+        Debug.Log(goal + " "+ player.transform.position);
+        if (ropeHookInstance.transform.position.y < player.transform.position.y) {
+            GameObject ropeInstance = Instantiate(ropeHook, new Vector3(goal.x, goal.y+1 , goal.z), Quaternion.identity, ropeHookInstance.transform);
+        }
+        Destroy(gameObject);
     }
 }
