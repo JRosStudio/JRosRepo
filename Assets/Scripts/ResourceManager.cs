dce4018ca2af709ddbfc1578e9aaa14b7db116d9
JRosStudio <jesusrosc@gmail.com>
2024-03-29 12:06:01 +0100

    Rope GUI and Text

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
     public int ropes = 0;
+    [SerializeField]
+    public TMP_Text ropeNumber;
+
+    private void Update()
+    {
+        ropeNumber.text = ropes.ToString();
+    }
 
     public void addRopes(int ropesAdded){
-        ropes = ropesAdded;
+        ropes += ropesAdded;
     }
     public int getCurrentRopes() {
         return ropes;
