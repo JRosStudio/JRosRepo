65c67111555ccb2a58f4295418e24a7bd86b552b
JRosStudio <jesusrosc@gmail.com>
2024-06-17 15:33:54 +0200

    Menu selection and interactivity
    Menu select first item when menu in and prevents interaction when menu out

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -16,9 +17,23 @@ public class MenuManager : MonoBehaviour
     [SerializeField]
     Player player;
 
+    [SerializeField]
+    GameObject firstButton;
+    [SerializeField]
+    GameObject secondButton;
+    [SerializeField]
+    GameObject thirdButton;
+
+    Button options;
+    Button tutorial;
+    Button controls;
+
     private void Start()
     {
         animationBlackScreen = BlackScreen.GetComponent<Animator>();
+        options = firstButton.GetComponent<Button>();
+        tutorial = secondButton.GetComponent<Button>();
+        controls = thirdButton.GetComponent<Button>();
     }
 
     public void pauseMenuHidden()
@@ -29,7 +44,10 @@ public class MenuManager : MonoBehaviour
         player.gamePaused = false;
     }
     public void pauseMenuOut() {
-
+        options.Select();
+        options.interactable = true;
+        tutorial.interactable = true;
+        controls.interactable = true;
         animation.SetInteger("PauseMenuState", 1);
     }
 
@@ -44,6 +62,10 @@ public class MenuManager : MonoBehaviour
     }
     public void pauseMenuBack()
     {
+
+        options.interactable = false;
+        tutorial.interactable = false;
+        controls.interactable = false;
         animation.SetInteger("PauseMenuState", 3);
     }
            
