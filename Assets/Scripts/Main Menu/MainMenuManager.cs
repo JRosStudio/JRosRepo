90a19ebf31293288c112a989754ef9638813219c
JRosStudio <jesusrosc@gmail.com>
2025-12-23 00:11:39 +0100

    Animacion to main menu y to select level
    Hay un error. Si se completa la animación de desaparecer los amigos, mientras estás en la pantalla de seleccionar nivel, se raya y no te deja volver al menu principal. Si pulsas el boton start durante la animación se la salta y pasa directamente al select level

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -10,20 +10,53 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] GameObject camara;
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject selectLevelMenu;
+    Animator cam_Animator;
+    public bool friendsGone;
+    public bool animationDone;
 
     public void Start()
     {
         camara.transform.position = mainMenuPos.transform.position;
+        cam_Animator = camara.GetComponent<Animator>();
+        friendsGone = false;
     }
     public void LoadGame() {
         SceneManager.LoadScene("Lvl1");
     }
 
 
-    public void selectLevel() {
-        camara.transform.position = selectLevelPos.transform.position;
-        mainMenu.SetActive(false);
-        selectLevelMenu.SetActive(true);
+    public void toSelectLevel() {
+        if (friendsGone) {
+            //camara.transform.position = selectLevelPos.transform.position;
+            mainMenu.SetActive(false);
+            selectLevelMenu.SetActive(true);
+            cam_Animator.SetTrigger("ToSelectLevel");
+            friendsGone = true;
+            animationDone = true;
+        }
+        friendsGone = true;
     }
 
+    public void selectLevel() {
+        if (friendsGone == false && animationDone == false)
+        {
+            //camara.transform.position = selectLevelPos.transform.position;
+            mainMenu.SetActive(false);
+            selectLevelMenu.SetActive(true);
+            cam_Animator.SetTrigger("ToSelectLevel");
+            friendsGone = true;
+            animationDone = true;
+        }
+    }
+
+    public void AnimationDone() {
+        animationDone = true;
+    }
+
+    public void ToMainMenu() {
+        mainMenu.SetActive(true);
+        selectLevelMenu.SetActive(false);
+        cam_Animator.SetTrigger("ToMainMenu");
+    }
+    
 }
