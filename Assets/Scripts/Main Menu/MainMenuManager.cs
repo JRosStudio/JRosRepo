5d98ba2496f6b3e90ee2e2e675c52a8aec6a1514
JRosStudio <jesusrosc@gmail.com>
2025-12-27 14:53:52 +0100

    Rresoluciones y pantalla completa en el menu setings/options

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -18,10 +18,16 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] GameObject controllerTuto;
     [SerializeField] GameObject textTuto_OBJ;
     [SerializeField] GameObject Button_Start_OBJ;
+    [SerializeField] GameObject Settings_Options;
+    [SerializeField] GameObject Settings_Controls;
+    [SerializeField] GameObject Settings_Tutorial;
+    public TMPro.TMP_Dropdown resolutionDropdown;
     private Button Button_Start;
     private TextMeshProUGUI textTuto;
-    
 
+    Resolution[] resolutions;
+    Resolution startingResolution;
+    List<Resolution> resolutions169;
 
     Animator cam_Animator;
     Animator actors_Animator;
@@ -39,49 +45,37 @@ public class MainMenuManager : MonoBehaviour
         textTuto = textTuto_OBJ.GetComponent<TextMeshProUGUI>();
         Button_Start = Button_Start_OBJ.GetComponent<Button>();
         Button_Start.Select();
+
+        //Resolution dropdown
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionsStringList = new List<string>();
+        resolutions169 = new List<Resolution>();
+        const float targetAspect = 16f / 9f;
+        const float tolerance = 0.01f;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            float aspect = (float)resolutions[i].width / resolutions[i].height;
+
+            if (Mathf.Abs(aspect - targetAspect) < tolerance)
+            {
+                
+                resolutions169.Add(resolutions[i]);
+                string res = resolutions[i].width + " x " + resolutions[i].height;
+                resolutionsStringList.Add(res);
+                startingResolution = resolutions[i];
+            }
+        }
+        resolutionDropdown.AddOptions(resolutionsStringList);
+        resolutionDropdown.value = resolutions169.Count;
+        Screen.SetResolution(startingResolution.width, startingResolution.height, Screen.fullScreen);
     }
 
     public void LoadGame() {
         SceneManager.LoadScene("Lvl1");
     }
-
-    public void openSettings() {
-
-        mainMenu.SetActive(false);
-        settingsMenu.SetActive(true);
-    }
-
-    public void closeSettings()
-    {
-        mainMenu.SetActive(true);
-        settingsMenu.SetActive(false);
-    }
-
-    public void closeExit()
-    {
-        mainMenu.SetActive(true);
-        exitMenu.SetActive(false);
-    }
-
-    public void openExit()
-    {
-        mainMenu.SetActive(false);
-        exitMenu.SetActive(true);
-    }
-
-    public void SettingsChangeImage() {
-        if (keyboardTuto.activeSelf) {
-            textTuto.text = "Watch keyboard";
-            controllerTuto.SetActive(true);
-            keyboardTuto.SetActive(false);
-        }
-        else if (controllerTuto.activeSelf)
-        {
-            textTuto.text = "Watch controller";
-            keyboardTuto.SetActive(true);
-            controllerTuto.SetActive(false);
-        }
-    } 
 
     public void toSelectLevel() {
         if (friendsGone) {
@@ -116,6 +110,77 @@ public class MainMenuManager : MonoBehaviour
         mainMenu.SetActive(true);
         selectLevelMenu.SetActive(false);
         cam_Animator.SetTrigger("ToMainMenu");
+    }
+
+
+
+    //SETTINGS
+    public void openSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void closeSettings()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void SettingsOptions() {
+        Settings_Options.SetActive(true);
+        Settings_Controls.SetActive(false);
+        Settings_Tutorial.SetActive(false);
+    }
+    public void SettingsControls()
+    {
+        Settings_Options.SetActive(false);
+        Settings_Controls.SetActive(true);
+        Settings_Tutorial.SetActive(false);
+    } 
+    
+    public void SettingsTutorial()
+    {
+        Settings_Options.SetActive(false);
+        Settings_Controls.SetActive(false);
+        Settings_Tutorial.SetActive(true);
+    }
+
+    public void SettingsChangeImage()
+    {
+        if (keyboardTuto.activeSelf)
+        {
+            textTuto.text = "Watch keyboard";
+            controllerTuto.SetActive(true);
+            keyboardTuto.SetActive(false);
+        }
+        else if (controllerTuto.activeSelf)
+        {
+            textTuto.text = "Watch controller";
+            keyboardTuto.SetActive(true);
+            controllerTuto.SetActive(false);
+        }
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions169[resolutionIndex - 1];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetFullScreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
+    }
+    //EXIT GAME
+
+    public void closeExit()
+    {
+        mainMenu.SetActive(true);
+        exitMenu.SetActive(false);
+    }
+
+    public void openExit()
+    {
+        mainMenu.SetActive(false);
+        exitMenu.SetActive(true);
     }
 
     public void CloseGame() {
